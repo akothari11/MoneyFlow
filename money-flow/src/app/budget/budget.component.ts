29743c20e84e62e6476b5acfea55dbcc580f3705
Anuj Kothari <anujkothari99@gmail.com>
2019-11-02 16:50:34 -0400

    implemented budget card

--- money-flow/src/app/budget/budget.component.ts
+++ money-flow/src/app/budget/budget.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-budget',
@@ -8,7 +9,21 @@ import { Location } from '@angular/common';
 })
 export class BudgetComponent implements OnInit {
 
-  constructor(private locationService: Location) { }
+  public salary: number;
+  public housingExpense: number;
+  public foodExpense: number;
+  public essentialBills: number;
+  public incomeExpenses: number;
+  public healthCare: number;
+  public minDebtPayments: number;
+
+  public invalidSalary: boolean;
+  public invalidHousingExpense: boolean;
+  public invalidFoodExpense: boolean;
+  public invalidIncomeExpense: boolean;
+  public invalidHealthCare: boolean;
+  public invalidDebtPayment: boolean;
+  constructor(private locationService: Location, private router: Router) { }
 
   ngOnInit() {
   }
@@ -18,6 +33,27 @@ export class BudgetComponent implements OnInit {
    */
   public goBack(): void {
     this.locationService.back();
+  }
+
+  public validateForm(): void {
+    if (!this.salary) {
+      this.invalidSalary = true;
+    }
+  }
+  public showChart(): void {
+    const data = {
+      budget: {
+        income: this.salary,
+        housingExpense: this.housingExpense,
+        foodExpense: this.foodExpense,
+        essentialBills: this.essentialBills,
+        incomeExpenses: this.incomeExpenses,
+        healthCare: this.healthCare,
+        minDebtPayments: this.minDebtPayments,
+      }
+    };
+    this.router.navigate(['emergency-fund', {budgetData: data}]);
+
   }
 
 }

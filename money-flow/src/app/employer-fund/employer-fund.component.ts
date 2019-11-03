d1bfbc04179f956a05bd6fa1a296bb7820586e0b
Anuj Kothari <anujkothari99@gmail.com>
2019-11-02 20:40:10 -0400

    added tip for employer funds question

--- money-flow/src/app/employer-fund/employer-fund.component.ts
+++ money-flow/src/app/employer-fund/employer-fund.component.ts
@@ -7,12 +7,19 @@ import { Location } from '@angular/common';
   styleUrls: ['./employer-fund.component.scss']
 })
 export class EmployerFundComponent implements OnInit {
-
+  public employerTip: boolean;
   constructor(private locationService: Location) { }
 
   ngOnInit() {
   }
 
+  public showEmployerTip(): void {
+    this.employerTip = true;
+  }
+
+  public showEmployerQuestion(): void {
+    this.employerTip = false;
+  }
   /**
    * Routes user to previous page
    */

aadec553b8b05218d6526e477e2e9e38bde3c9f9
Anuj Kothari <anujkothari99@gmail.com>
2019-11-03 00:21:59 -0400

    implemented savings section

--- money-flow/src/app/savings/savings.component.ts
+++ money-flow/src/app/savings/savings.component.ts
@@ -8,9 +8,117 @@ import { Location } from '@angular/common';
 })
 export class SavingsComponent implements OnInit {
 
+  public savingsQuestionCard: boolean;
+  public savingsAdvice: boolean;
+  public savingsRetirementQuestion: boolean;
+  public savings401kQuestion: boolean;
+  public healthCareQuestion: boolean;
+  public savings401kAdvice: boolean;
+  public savingsIndividual401k: boolean;
+  public savingsHealthCareAdvice: boolean;
+  public savingsChildCareQuestion: boolean;
+  public savingsChildCareAdvice: boolean;
+  public finalCard: boolean;
   constructor(private locationService: Location) { }
 
   ngOnInit() {
+  }
+
+  public showSavingsQuestion() {
+    this.savingsQuestionCard = true;
+  }
+  public answerSavingsQuestionTrue() {
+    this.savingsAdvice = true;
+    this.savingsRetirementQuestion = false;
+    this.savings401kQuestion = false;
+    this.healthCareQuestion = false;
+    this.savings401kAdvice = false;
+    this.savingsIndividual401k = false;
+    this.savingsHealthCareAdvice = false;
+    this.savingsChildCareQuestion = false;
+    this.savingsChildCareAdvice = false;
+    this.finalCard = false;
+  }
+  public answerSavingsQuestionFalse() {
+    this.savingsAdvice = false;
+    this.savingsRetirementQuestion = true;
+    this.savings401kQuestion = false;
+    this.healthCareQuestion = false;
+    this.savings401kAdvice = false;
+    this.savingsIndividual401k = false;
+    this.savingsHealthCareAdvice = false;
+    this.savingsChildCareQuestion = false;
+    this.savingsChildCareAdvice = false;
+    this.finalCard = false;
+  }
+
+  public answerSavingsRetirementQuestionTrue() {
+    this.healthCareQuestion = true;
+    this.savings401kQuestion = false;
+    this.savings401kAdvice = false;
+    this.savingsIndividual401k = false;
+    this.savingsHealthCareAdvice = false;
+    this.savingsChildCareQuestion = false;
+    this.savingsChildCareAdvice = false;
+    this.finalCard = false;
+  }
+
+  public answerSavingsRetirementQuestionFalse() {
+    this.healthCareQuestion = false;
+    this.savings401kQuestion = true;
+    this.savings401kAdvice = false;
+    this.savingsIndividual401k = false;
+    this.savingsHealthCareAdvice = false;
+    this.savingsChildCareQuestion = false;
+    this.savingsChildCareAdvice = false;
+    this.finalCard = false;
+  }
+
+  public answerEmployer401kTrue() {
+
+    this.healthCareQuestion = false;
+    this.savings401kAdvice = true;
+    this.savingsIndividual401k = false;
+    this.savingsHealthCareAdvice = false;
+    this.savingsChildCareQuestion = false;
+    this.savingsChildCareAdvice = false;
+    this.finalCard = false;
+  }
+
+  public answerEmployer401kFalse() {
+    this.healthCareQuestion = false;
+    this.savings401kAdvice = false;
+    this.savingsIndividual401k = true;
+    this.savingsHealthCareAdvice = false;
+    this.savingsChildCareQuestion = false;
+    this.savingsChildCareAdvice = false;
+    this.finalCard = false;
+  }
+
+  public answerHealthQuestionTrue() {
+
+    this.savingsHealthCareAdvice = true;
+    this.savingsChildCareQuestion = false;
+    this.savingsChildCareAdvice = false;
+    this.finalCard = false;
+  }
+
+  public answerHealthQuestionFalse() {
+    this.savingsHealthCareAdvice = false;
+    this.savingsChildCareQuestion = true;
+    this.savingsChildCareAdvice = false;
+    this.finalCard = false;
+  }
+
+  public answerChildCareQuestionTrue() {
+
+    this.savingsChildCareAdvice = true;
+    this.finalCard = false;
+  }
+
+  public answerChildCareQuestionFalse() {
+    this.finalCard = true;
+    this.savingsChildCareAdvice = false;
   }
 
   /**

79c1b0f180b8b5fc3cf8b614fa47a9afb6bc79ac
Anuj Kothari <anujkothari99@gmail.com>
2019-11-02 22:27:12 -0400

    implemented cards for efund and debt sections

--- money-flow/src/app/debt/debt.component.ts
+++ money-flow/src/app/debt/debt.component.ts
@@ -7,12 +7,49 @@ import { Location } from '@angular/common';
   styleUrls: ['./debt.component.scss']
 })
 export class DebtComponent implements OnInit {
-
+  public eFundTip: boolean;
+  public debtTip: boolean;
+  public moderateInterestDebtQuestion: boolean;
+  public moderateInterestDebtTip: boolean;
+  public debtHeader = true;
+  public moderateDebtHeader: boolean;
+  public seenHighDebtTip: boolean;
   constructor(private locationService: Location) { }
 
   ngOnInit() {
   }
 
+  public showEFundTip() {
+    this.eFundTip = true;
+    this.debtTip = false;
+    this.debtHeader = false;
+    this.moderateDebtHeader = false;
+    this.moderateInterestDebtTip = false;
+    this.moderateInterestDebtQuestion = false;
+  }
+
+  public showDebtTip() {
+    this.debtTip = true;
+    this.seenHighDebtTip = true;
+  }
+
+  public showDebtQuestion() {
+    this.debtTip = false;
+    this.eFundTip = false;
+    this.debtHeader = true;
+  }
+
+  public showModerateInterestQuestion() {
+    this.eFundTip = false;
+    this.debtTip = false;
+    this.moderateInterestDebtQuestion = true;
+    this.moderateDebtHeader = true;
+  }
+
+  public showModerateInterestDebtTip() {
+    this.moderateInterestDebtQuestion = false;
+    this.moderateInterestDebtTip = true;
+  }
   /**
    * Routes user to previous page
    */
